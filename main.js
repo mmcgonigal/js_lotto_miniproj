5e50e69d30a50f18f203fcb5fe67602938f06ba3
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-30 20:16:02 -0500

    lotto number generate function

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 'use strict'
+
+let numbers = document.querySelector(".numbers")
+let drawButton = document.querySelector("#draw")
+let resetButton = document.querySelector("#reset")
+
+const lottoNumbers= [];
+const colors = ["red","yellow","blue","green","orange","purple"]; // for balls color scheme
+
+function showNumber(number){
+    const eachNumDiv =  document.createElement("div")
+    let colorIndex =  Math.floor(number / 10);
+    colors[colorIndex]
+    eachNumDiv.classList.add('eachNum')
+    eachNumDiv.style.backgroundColor = colors[colorIndex]
+    eachNumDiv.textContent = number
+    numbers.appendChild(eachNumDiv)
+}
+
+drawButton.addEventListener("click",function(){
+    while(lottoNumbers.length < 6){
+       let randomNum =  Math.floor( Math.random() * 54 ) + 1;
+        if(lottoNumbers.indexOf(randomNum)=== -1){
+            //만약에,주어진 값 , 이경우는 랜덤넘이, 로또넘버 배열의 몇번째에 , 즉 몇번째 인덱스에 존재하는지 확인 하는데, 만약 그 값이 배열안에 존재 하지 않을 경우는 보통 -1을 반환 함. 즉 해석하자면, 로또넘버스 배열안에, 랜덤넘의 인덱스가 , (랜덤넘이 들어 있는 인덱스)가 존재하지 않아서 -1 일 경우,
+            lottoNumbers.push(randomNum)
+            showNumber(randomNum)
+        }
+
+    }
+    console.log(lottoNumbers)
+
+})
+resetButton.addEventListener("click",function(){
+    numbers.innerHTML = "";
+
+})
